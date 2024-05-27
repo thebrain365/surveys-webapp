288f7e298d4ffd6eb1274108c56116ab8e800797
Muano <muano.m365@gmail.com>
2024-05-27 13:08:33 +0200

    Added Prisma to the project and it's connected to the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import Image from "next/image";
+import { PrismaClient, books } from "@prisma/client";
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+export default async function Home() {
+   let books: books[] = [];
+
+   await prisma.books
+      .findMany()
+      .then((data) => {
+         books = [...data];
+      })
+      .catch((err) => {
+         console.log(err);
+      });
+
    return (
       <main>
          <h1>Muano Makhokha</h1>
+         <div>
+            {books.map((book: books) => (
+               <div key={book.id}>
+                  <h1>{book.title}</h1>
+                  <p>{book.author}</p>
+               </div>
+            ))}
+         </div>
       </main>
    );
 }
